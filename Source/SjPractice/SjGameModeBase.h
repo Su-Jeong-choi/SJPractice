b75225bc1567733029724ac23481054f0da3d24f
Su-Jeong-choi <kitchonbear@gmail.com>
2024-01-01 11:47:50 +0900

    Unreal C++ Practice

---
+++ Source/SjPractice/SjGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SJPRACTICE_API ASjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
